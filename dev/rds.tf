4fb3836a74f231b7c6e575220c20f79d3625eaf3
sivakumarej <sivakumar.s@easyjet.com>
2024-01-31 06:26:46 +0000

    Updating subnet ids

--- dev/rds.tf
+++ dev/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = var.rds_subnet_group_name
-  subnet_ids = [module.vpc.private_subnets[0].id,module.vpc.private_subnets[1].id]
+  subnet_ids = module.vpc.private_subnets
 
   tags = {
     Name = "dev" 
